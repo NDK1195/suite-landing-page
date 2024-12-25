c9513136dd51273607dcdcacb44e8ff681608c85
ndk1195 <ndkhoa1195@gmail.com>
2024-12-25 13:51:15 +0700

    add project structure

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    extend: {
+      backgroundImage: {
+        hover:
+          "linear-gradient(135deg, #A060FF 0%, #CB30E3 49.21%, #FFA84E 100%)",
+      },
+    },
+  },
+  plugins: [],
+};
