e493d6976a95edd853be98bb6d2136e713e54ddb
ndk1195 <ndkhoa1195@gmail.com>
2024-12-25 15:49:32 +0700

    finish project

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,15 @@ module.exports = {
         hover:
           "linear-gradient(135deg, #A060FF 0%, #CB30E3 49.21%, #FFA84E 100%)",
       },
+      colors: {
+        "#172339": "#172339",
+        "#49566D": "#49566D",
+        "#F3EDE7": "#F3EDE7",
+        "#FAF8F6": "#FAF8F6",
+        "#A060FF": "#A060FF",
+        "#CB30E3": "#CB30E3",
+        "#FFA84E": "#FFA84E",
+      },
     },
   },
   plugins: [],
